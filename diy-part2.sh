87e446b9ae50216702774944a6a0757aa0338491
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-04-02 09:23:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/generic-tp-link.mk
-sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/tiny-tp-link.mk
+#sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/generic-tp-link.mk
+#sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/tiny-tp-link.mk
 
-sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/common-tp-link.mk
-sudo sed -i 's/TPLINK_FLASHLAYOUT := 8M/TPLINK_FLASHLAYOUT := 16M/g' target/linux/ar71xx/image/common-tp-link.mk
-sudo sed -i 's/IMAGE_SIZE := 7936k/IMAGE_SIZE := 15872k/g' target/linux/ar71xx/image/common-tp-link.mk
+#sudo sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/common-tp-link.mk
+#sudo sed -i 's/TPLINK_FLASHLAYOUT := 8M/TPLINK_FLASHLAYOUT := 16M/g' target/linux/ar71xx/image/common-tp-link.mk
+#sudo sed -i 's/IMAGE_SIZE := 7936k/IMAGE_SIZE := 15872k/g' target/linux/ar71xx/image/common-tp-link.mk
